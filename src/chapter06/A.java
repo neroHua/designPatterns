d57495d2a0666d010149b11854beeb74152e2bec
neroHua <2676337735@qq.com>
2019-02-01 10:59:08 +0800

    add builder pattern

---
+++ src/chapter06/A.java
@@ -0,0 +1,84 @@
+package chapter06;
+
+public class A {
+    
+    private Long id;
+    private String name;
+    private Integer age;
+    private String address;
+    private String company;
+    private String school;
+    private String phoneNumber;
+    private String idCardNumber;
+    private String passPortNumber;
+    private Double height;
+    private Double weight;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCompany() {
+        return company;
+    }
+    public void setCompany(String company) {
+        this.company = company;
+    }
+    public String getSchool() {
+        return school;
+    }
+    public void setSchool(String school) {
+        this.school = school;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getIdCardNumber() {
+        return idCardNumber;
+    }
+    public void setIdCardNumber(String idCardNumber) {
+        this.idCardNumber = idCardNumber;
+    }
+    public String getPassPortNumber() {
+        return passPortNumber;
+    }
+    public void setPassPortNumber(String passPortNumber) {
+        this.passPortNumber = passPortNumber;
+    }
+    public Double getHeight() {
+        return height;
+    }
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+    public Double getWeight() {
+        return weight;
+    }
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+    
+}
---
+++ src/chapter06/ABuilder.java
@@ -0,0 +1,61 @@
+package chapter06;
+
+public class ABuilder {
+
+    private A a;
+    
+    public ABuilder builder() {
+        a = new A();
+        return this;
+    }
+    
+    public A build() {
+        return a;
+    }
+
+    public ABuilder id(Long id) {
+        a.setId(id);
+        return this;
+    }
+    public ABuilder name(String name) {
+        a.setName(name);
+        return this;
+    }
+    public ABuilder age(Integer age) {
+        a.setAge(age);
+        return this;
+    }
+    public ABuilder address(String address) {
+        a.setAddress(address);
+        return this;
+    }
+    public ABuilder company(String company) {
+        a.setCompany(company);
+        return this;
+    }
+    public ABuilder school(String school) {
+        a.setSchool(school);
+        return this;
+    }
+    public ABuilder phoneNumber(String phoneNumber) {
+        a.setPhoneNumber(phoneNumber);
+        return this;
+    }
+    public ABuilder idCardNumber(String idCardNumber) {
+        a.setIdCardNumber(idCardNumber);
+        return this;
+    }
+    public ABuilder passPortNumber(String passPortNumber) {
+        a.setPassPortNumber(passPortNumber);
+        return this;
+    }
+    public ABuilder height(Double height) {
+        a.setHeight(height);
+        return this;
+    }
+    public ABuilder weight(Double weight) {
+        a.setWeight(weight);
+        return this;
+    }
+
+}
