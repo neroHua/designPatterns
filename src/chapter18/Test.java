dfc283b4e0cd3e7b0868ecba48a507b205b5649a
neroHua <2676337735@qq.com>
2019-02-21 16:03:33 +0800

    primary skip interpreter pattern

---
+++ src/chapter18/Test.java
@@ -0,0 +1,17 @@
+package chapter18;
+
+/**
+ * 
+ * 解释器模式
+ * 
+ * @author 滑德友
+ * @time 2019年2月21日16:02:24
+ *
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("解释器模式的应用范围较窄！");
+    }
+
+}
