b02c7f9fc8409879884a52fa8f03d182b1d0bdeb
neroHua <2676337735@qq.com>
2019-03-04 19:15:45 +0800

    update chapter05

--- src/chapter05/Client.java
+++ src/chapter05/Client.java
@@ -2,13 +2,13 @@ package chapter05;
 
 /**
  * 
- * 单例模式
+ * 单例模式(singleton)
  * 单例模式的写法有很多种，尽量不要重复创建，高并发时可以以线程安全的方式创建
  * 
  * @author 滑德友
  * @time 2019年2月1日10:39:50
  *
  */
-public class Test {
+public class Client {
 
 }
