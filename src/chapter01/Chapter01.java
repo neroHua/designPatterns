35ce60d148d9f1b036697565e1f544cba5cd9ed9
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-05 17:30:36 +0800

    Create Chapter01.java

---
+++ src/chapter01/Chapter01.java
@@ -0,0 +1,91 @@
+package chapter01;
+
+/**
+ * introduction
+ * 简介
+ *
+ * 1.1 what is a design pattern ?
+ * 1.1 什么是设计模式 ？
+ *
+ *      Each pattern describes a problem which occurs over and over again in our environment, and then describes the core of the solution to that problem, in such a way that you can use this solution a million times over, without ever doing it the same way twice
+ *      每一种设计模式描述了解决一种特定问题的核心套路，这种套路可以帮助我们解决特定问题
+ *
+ * 1.2 describing Design Patterns
+ * 1.2 描述设计模式
+ *
+ *      1.2.1 pattern name and Classification
+ *      1.2.1 名称和归类
+ *
+ *          名称是非常重要的，他能非常直观的体现设计模式的功能和作用。
+ *
+ *      1.2.2 Intent
+ *      1.2.2 意图
+ *
+ *          解释设计模式是干什么的？他的原理和意图是什么？他能解决什么样的特定问题？
+ *
+ *      1.2.3 Also Known As
+ *      1.2.3 其他名称
+ *
+ *      1.2.4 Structure
+ *      1.2.4 结构
+ *
+ *          用形象的图表来解释设计模式。不限于使用 建模技术Object Modeling Technique 和 交互图interaction diagrams。
+ *
+ *      1.2.5 Consequences
+ *      1.2.5 潜在危害
+ *
+ *          描述设计模式的局限性，已及不当使用的严重后果
+ *
+ * 1.3 How Design Patterns Solve Design Problems
+ * 1.3 设计模式如何解决问题
+ *
+ *      1.3.1 Finding Appropriate Objects
+ *      1.3.1 找合适的对象
+ *
+ *          进行抽象，理清业务中的逻辑关系，进行合理的建模
+ *
+ *      1.3.2 Specifying Object Interfaces
+ *      1.3.2 确定对象的粒度
+ *
+ *      1.3.3 Specifying Object Interfaces
+ *      1.3.3 确定对象的接口
+ *
+ *      1.3.4 Specifying Object Implementations
+ *      1.3.4 确定对象的实现
+ *
+ *      1.3.5 Putting Reuse Mechanisms to Work
+ *      1.3.5 让重用机制工作起来
+ *
+ *         three common techniques for reusing in object-oriented system is class inheritance and object composition and parameterized type
+ *         三个在面向对象编程的系统里，常用的重复利用代码的技术：类的继承，对象的组合，参数化类型。
+ *
+ *      1.3.6 Designing for Change
+ *      1.3.6 为变化而设计
+ *
+ *          每一种设计模式，都会预留可控变化，使得在变化的方向上，满足不修改老的代码，只要上新的代码，即开闭原则
+ *
+ * 1.4 How to Select a Design Pattern
+ * 1.4 如何选择一个设计模式
+ *
+ *      1.4.1 consider how pattern solve design problems
+ *      1.4.1 考虑设计模式是如何工作的
+ *
+ *      1.4.2 Scan Intent sections
+ *      1.4.2 考虑设计的意图
+ *
+ *          考虑设计设计模式能解决的问题和你的是否相似
+ *
+ *      1.4.3 Consider what should be variable in your design
+ *      1.4.3 考虑设计中可能存在的变化方向
+ *
+ *          在变化的方向上是否满足开闭原则
+ *
+ *      1.4.4 consider consequences
+ *      1.4.4 考虑后果
+ *
+ * 1.5 How to Use a Design Pattern
+ * 1.5 如何使用一个设计模式
+ *
+ */
+public class Chapter01 {
+}
