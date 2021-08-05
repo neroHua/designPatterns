ba572185b040096dacf9e1e070cecfbed2be695d
neroHua <2676337735@qq.com>
2021-08-05 22:52:07 +0800

    add row

---
+++ src/chapter02/data/Row.java
@@ -0,0 +1,43 @@
+package chapter02.data;
+
+import java.util.List;
+
+public class Row implements Glyph {
+
+    private List<Glyph> children;
+
+    public Row(List<Glyph> children) {
+        this.children = children;
+    }
+
+    @Override
+    public void draw(Window window) {
+        for (Glyph glyph : children) {
+            glyph.draw(window);
+        }
+    }
+
+    @Override
+    public boolean intersects(Point point) {
+        for (Glyph glyph : children) {
+            if (glyph.intersects(point)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void insert(Glyph glyph, int type) {
+
+    }
+
+    public List<Glyph> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Glyph> children) {
+        this.children = children;
+    }
+}
