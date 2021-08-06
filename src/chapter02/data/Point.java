ecd21fa6cce74e6b2adcc0066693930e46d57fac
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-06 10:52:11 +0800

    Update Point.java

--- src/chapter02/data/Point.java
+++ src/chapter02/data/Point.java
@@ -1,4 +1,31 @@
 package chapter02.data;
 
 public class Point {
+    private long x;
+
+    private long y;
+
+    public Point() {
+    }
+
+    public Point(long x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public void setY(long y) {
+        this.y = y;
+    }
 }
