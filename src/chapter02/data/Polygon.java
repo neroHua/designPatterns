e733ef32f8f8859fdf6956c75ad062b052cfda18
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-06 11:39:50 +0800

    Update Polygon.java

--- src/chapter02/data/Polygon.java
+++ src/chapter02/data/Polygon.java
@@ -17,4 +17,19 @@ public class Polygon implements Glyph {
 
     }
 
+    @Override
+    public void remove(Glyph glyph) {
+
+    }
+
+    @Override
+    public Glyph child(int index) {
+        return null;
+    }
+
+    @Override
+    public Glyph parent() {
+        return null;
+    }
+
 }
