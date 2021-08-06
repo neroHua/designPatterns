d2abde85b9f7ec7110afe4faf4b8772233035066
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-06 17:21:24 +0800

    Create TexFormat.java

---
+++ src/chapter02/format/TexFormat.java
@@ -0,0 +1,19 @@
+package chapter02.format;
+
+import chapter02.data.Glyph;
+
+public class TeXFormat implements Format {
+
+    private Glyph glyph;
+
+    @Override
+    public void format() {
+
+    }
+
+    @Override
+    public void setGlyph(Glyph glyph) {
+
+    }
+
+}
