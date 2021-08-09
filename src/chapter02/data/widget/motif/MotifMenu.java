38b1b355eba9c794f9715cc37447eb7f8e9fdc03
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-09 14:25:12 +0800

    Create MotifMenu.java

---
+++ src/chapter02/data/widget/motif/MotifMenu.java
@@ -0,0 +1,17 @@
+package chapter02.data.widget.motif;
+
+import chapter02.data.widget.Menu;
+
+public class MotifMenu extends Menu {
+
+    @Override
+    public void popUp() {
+
+    }
+
+    @Override
+    public void popDown() {
+
+    }
+
+}
