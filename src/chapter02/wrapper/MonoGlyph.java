ceeecf8d1b336e1c2149bad451e1b75b27e32f75
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-09 11:24:56 +0800

    Create MonoGlyph.java

---
+++ src/chapter02/wrapper/MonoGlyph.java
@@ -0,0 +1,15 @@
+package chapter02.wrapper;
+
+import chapter02.data.Glyph;
+import chapter02.data.Window;
+
+public abstract class MonoGlyph implements Glyph {
+
+    private Glyph glyph;
+
+    @Override
+    public void draw(Window window) {
+        glyph.draw(window);
+    }
+
+}
