4f44bd8168f13d1ed022614a387acf8146064f6a
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-09 11:25:40 +0800

    Create Border.java

---
+++ src/chapter02/wrapper/Border.java
@@ -0,0 +1,42 @@
+package chapter02.wrapper;
+
+import chapter02.data.Glyph;
+import chapter02.data.Point;
+import chapter02.data.Window;
+
+public class Border extends MonoGlyph {
+
+    @Override
+    public void draw(Window window) {
+        super.draw(window);
+        drawBorder(window);
+    }
+
+    public void drawBorder(Window window) {
+    }
+
+    @Override
+    public boolean intersects(Point point) {
+        return false;
+    }
+
+    @Override
+    public void insert(Glyph glyph, int index) {
+
+    }
+
+    @Override
+    public void remove(Glyph glyph) {
+
+    }
+
+    @Override
+    public Glyph child(int index) {
+        return null;
+    }
+
+    @Override
+    public Glyph parent() {
+        return null;
+    }
+}
