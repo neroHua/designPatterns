cda52e9b2bb7ff57e2306e7d146f27d9b4376929
neroHua <2676337735@qq.com>
2021-08-09 23:30:07 +0800

    add three kind window

---
+++ src/chapter02/os/DialogWindow.java
@@ -0,0 +1,24 @@
+package chapter02.os;
+
+import chapter02.data.Character;
+import chapter02.data.Polygon;
+import chapter02.data.Rectangle;
+
+public class DialogWindow implements Window {
+
+    @Override
+    public void drawCharacter(Character character) {
+
+    }
+
+    @Override
+    public void drawRectangle(Rectangle rectangle) {
+
+    }
+
+    @Override
+    public void drawPolygon(Polygon polygon) {
+
+    }
+
+}
