3e6c3d39bef996d3bee58c748e30569fc59f25c8
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-09 14:12:07 +0800

    Create MacMenu.java

---
+++ src/chapter02/data/widget/mac/MacMenu.java
@@ -0,0 +1,17 @@
+package chapter02.data.widget.mac;
+
+import chapter02.data.widget.Menu;
+
+public class MacMenu extends Menu {
+
+    @Override
+    public void popUp() {
+
+    }
+
+    @Override
+    public void popDown() {
+
+    }
+
+}
