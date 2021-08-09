3fdd0b74202b7a1a258fa6e4c52f1acfc17a10ef
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-09 14:12:50 +0800

    Create PMMenu.java

---
+++ src/chapter02/data/widget/pm/PMMenu.java
@@ -0,0 +1,17 @@
+package chapter02.data.widget.pm;
+
+import chapter02.data.widget.Menu;
+
+public class PMMenu extends Menu {
+
+    @Override
+    public void popUp() {
+
+    }
+
+    @Override
+    public void popDown() {
+
+    }
+
+}
