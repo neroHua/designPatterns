e78bce91cc35c415626329fa2c37b7e0924a7f4a
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-09 14:14:03 +0800

    Create PMScrollBar.java

---
+++ src/chapter02/data/widget/pm/PMScrollBar.java
@@ -0,0 +1,12 @@
+package chapter02.data.widget.pm;
+
+import chapter02.data.widget.ScrollBar;
+
+public class PMScrollBar extends ScrollBar {
+
+    @Override
+    public void scrollTo(int y) {
+
+    }
+
+}
