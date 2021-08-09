15782ddc08f4152b6bf130141fb3df985131323d
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-09 14:11:32 +0800

    Create MacScrollBar.java

---
+++ src/chapter02/data/widget/mac/MacScrollBar.java
@@ -0,0 +1,12 @@
+package chapter02.data.widget.mac;
+
+import chapter02.data.widget.ScrollBar;
+
+public class MacScrollBar extends ScrollBar {
+
+    @Override
+    public void scrollTo(int y) {
+
+    }
+
+}
