521239d18da07e61befb3187a582cb68f57b6f98
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-09 14:10:14 +0800

    Create Button.java

---
+++ src/chapter02/data/widget/Button.java
@@ -0,0 +1,41 @@
+package chapter02.data.widget;
+
+import chapter02.data.Glyph;
+import chapter02.data.Point;
+import chapter02.data.Window;
+
+public abstract class Button implements Glyph {
+
+    @Override
+    public void draw(Window window) {
+
+    }
+
+    @Override
+    public boolean intersects(Point point) {
+        return false;
+    }
+
+    @Override
+    public void insert(Glyph glyph, int index) {
+
+    }
+
+    @Override
+    public void remove(Glyph glyph) {
+
+    }
+
+    @Override
+    public Glyph child(int index) {
+        return null;
+    }
+
+    @Override
+    public Glyph parent() {
+        return null;
+    }
+
+    public abstract void press();
+
+}
