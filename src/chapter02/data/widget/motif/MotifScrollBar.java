9106e8a021ad9611f8af4cb3e77b92112fe71e66
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-09 14:25:45 +0800

    Create MotifScrollBar.java

---
+++ src/chapter02/data/widget/motif/MotifScrollBar.java
@@ -0,0 +1,12 @@
+package chapter02.data.widget.motif;
+
+import chapter02.data.widget.ScrollBar;
+
+public class MotifScrollBar extends ScrollBar {
+
+    @Override
+    public void scrollTo(int y) {
+
+    }
+
+}
