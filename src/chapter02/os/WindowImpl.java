5506d05f4162546dad3c536d8f524dfaf9f212ca
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-10 15:47:30 +0800

    Update WindowImpl.java

--- src/chapter02/os/WindowImpl.java
+++ src/chapter02/os/WindowImpl.java
@@ -1,30 +1,35 @@
 package chapter02.os;
 
 import chapter02.data.Character;
+import chapter02.data.Point;
 import chapter02.data.Polygon;
 import chapter02.data.Rectangle;
+import chapter02.os.impl.XWindowImpl;
 
 public abstract class WindowImpl extends Window {
 
-    private Window concreteWindow;
+    private WindowImpl concreteWindowImpl;
 
     public WindowImpl() {
-        
+        // init by os
+        concreteWindowImpl = new XWindowImpl();
     }
 
     @Override
     public void drawCharacter(Character character) {
-        concreteWindow.drawCharacter(character);
+        concreteWindowImpl.drawCharacter(character);
     }
 
     @Override
     public void drawRectangle(Rectangle rectangle) {
-        concreteWindow.drawRectangle(rectangle);
+        concreteWindowImpl.drawRectangle(rectangle.getPoint1(), rectangle.getPoint2());
     }
 
     @Override
     public void drawPolygon(Polygon polygon) {
-        concreteWindow.drawPolygon(polygon);
+        concreteWindowImpl.drawPolygon(polygon);
     }
+
+    public abstract void drawRectangle(Point point1, Point point2);
 
 }
