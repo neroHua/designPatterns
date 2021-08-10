435cfe9c83535e5d07c9d8112d4d3693778f31be
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-10 15:51:24 +0800

    Create MacWindowImpl.java

---
+++ src/chapter02/os/impl/MacWindowImpl.java
@@ -0,0 +1,13 @@
+package chapter02.os.impl;
+
+import chapter02.data.Point;
+import chapter02.os.WindowImpl;
+
+public class MacWindowImpl extends WindowImpl {
+
+    @Override
+    public void drawRectangle(Point point1, Point point2) {
+        // draw rectangle
+    }
+
+}
