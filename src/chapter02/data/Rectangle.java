05dc233f2a62049919060bc25a4a6ba09b30a06c
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-10 15:38:40 +0800

    Update Rectangle.java

--- src/chapter02/data/Rectangle.java
+++ src/chapter02/data/Rectangle.java
@@ -7,6 +7,10 @@ public class Rectangle implements Glyph {
 
     private Format format;
 
+    private Point point1;
+
+    private Point point2;
+
     public Rectangle(Format format) {
         this.format = format;
     }
@@ -41,4 +45,19 @@ public class Rectangle implements Glyph {
         return null;
     }
 
+    public Point getPoint1() {
+        return point1;
+    }
+
+    public void setPoint1(Point point1) {
+        this.point1 = point1;
+    }
+
+    public Point getPoint2() {
+        return point2;
+    }
+
+    public void setPoint2(Point point2) {
+        this.point2 = point2;
+    }
 }
