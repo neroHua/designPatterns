bc8f5cb0004c9f87353796adc746bcaeca56dc39
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-13 16:58:10 +0800

    Create visitor.java

---
+++ src/chapter02/data/visitor/visitor.java
@@ -0,0 +1,18 @@
+package chapter02.data.visitor;
+
+import chapter02.data.*;
+import chapter02.data.Character;
+
+public interface Visitor {
+
+    void visitCharacter(Character character);
+
+    void visitPoint(Point point);
+
+    void visitPolygon(Polygon polygon);
+
+    void visitRectangle(Rectangle rectangle);
+
+    void visitRow(Row row);
+
+}
