75bf1ad6897d057b01daddb8be9adac1739e8246
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-13 16:59:32 +0800

    Create HyphenationVisitor.java

---
+++ src/chapter02/data/visitor/HyphenationVisitor.java
@@ -0,0 +1,33 @@
+package chapter02.data.visitor;
+
+import chapter02.data.*;
+import chapter02.data.Character;
+
+public class HyphenationVisitor implements Visitor {
+
+    @Override
+    public void visitCharacter(Character character) {
+
+    }
+
+    @Override
+    public void visitPoint(Point point) {
+
+    }
+
+    @Override
+    public void visitPolygon(Polygon polygon) {
+
+    }
+
+    @Override
+    public void visitRectangle(Rectangle rectangle) {
+
+    }
+
+    @Override
+    public void visitRow(Row row) {
+
+    }
+
+}
