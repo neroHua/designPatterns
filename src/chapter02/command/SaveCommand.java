4d6991a2a474ac2a3059454e5c4801c4a7609758
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-13 10:15:56 +0800

    Create SaveCommand.java

---
+++ src/chapter02/command/SaveCommand.java
@@ -0,0 +1,15 @@
+package chapter02.command;
+
+public class SaveCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void unExecute() {
+
+    }
+
+}
