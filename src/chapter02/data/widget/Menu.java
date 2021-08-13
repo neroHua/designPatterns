7486caf851199088b3a16cd0e1c1faf99e108423
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-13 10:11:12 +0800

    Update Menu.java

--- src/chapter02/data/widget/Menu.java
+++ src/chapter02/data/widget/Menu.java
@@ -1,10 +1,13 @@
 package chapter02.data.widget;
 
+import chapter02.command.Command;
 import chapter02.data.Glyph;
 import chapter02.data.Point;
 import chapter02.os.Window;
 
 public abstract class Menu implements Glyph {
+
+    private Command command;
 
     @Override
     public void draw(Window window) {
@@ -39,4 +42,8 @@ public abstract class Menu implements Glyph {
     public abstract void popUp();
 
     public abstract void popDown();
+
+    public void click() {
+        command.execute();
+    }
 }
