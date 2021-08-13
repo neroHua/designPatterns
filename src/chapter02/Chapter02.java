d91c4147c5618246f78ee599d8b13ab3533bf45a
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-13 18:23:58 +0800

    Update Chapter02.java

--- src/chapter02/Chapter02.java
+++ src/chapter02/Chapter02.java
@@ -2,7 +2,13 @@ package chapter02;
 
 /**
  * a example for text and graph editor application
- * Composite Pattern can be used for document physical structure
+ * 一个处理文本和图形应用的例子
+ *
+ * now I believe this chapter should be the most import part in this book
+ * 现在，我相信这一个章节是本书最重要的部分
+ *
+ * it shows how to design for change and what's trade-off in design, so that comes design pattern
+ * 本章节演示了在设计中的权衡以及应对变化，而在这处理的过程中产生了设计模式
  */
 public class Chapter02 {
 }
