106246d1da25fe8bf73305705c4bee3cfcce37e4
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-13 15:38:49 +0800

    Create ListIterator.java

---
+++ src/chapter02/data/iterator/ListIterator.java
@@ -0,0 +1,27 @@
+package chapter02.data.iterator;
+
+import chapter02.data.Glyph;
+
+public class ListIterator implements Iterator {
+
+    @Override
+    public Glyph first() {
+        return null;
+    }
+
+    @Override
+    public Glyph next() {
+        return null;
+    }
+
+    @Override
+    public boolean isDone() {
+        return false;
+    }
+
+    @Override
+    public Glyph currentItem() {
+        return null;
+    }
+
+}
