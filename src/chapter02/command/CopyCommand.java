700b396eac98ca1a66d247202d2d038b91f3d24f
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-08-13 10:14:48 +0800

    Create CopyCommand.java

---
+++ src/chapter02/command/CopyCommand.java
@@ -0,0 +1,15 @@
+package chapter02.command;
+
+public class CopyCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void unExecute() {
+
+    }
+
+}
