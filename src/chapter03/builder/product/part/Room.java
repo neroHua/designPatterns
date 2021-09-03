219420be0627721e44d77a75af1161ab39062b92
neroHua <38644203+neroHua@users.noreply.github.com>
2021-09-03 15:47:42 +0800

    Create Room.java

---
+++ src/chapter03/builder/product/part/Room.java
@@ -0,0 +1,19 @@
+package chapter03.builder.product.part;
+
+public class Room {
+
+    Integer roomNumber;
+
+    public Room(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+}
