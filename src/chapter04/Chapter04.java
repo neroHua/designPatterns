523fcdf38ec9b4170633d42cd855bfdf0cb87ba7
neroHua <38644203+neroHua@users.noreply.github.com>
2021-10-25 18:14:51 +0800

    Update Chapter04.java

--- src/chapter04/Chapter04.java
+++ src/chapter04/Chapter04.java
@@ -1,34 +1,27 @@
-package chapter04.adapter;
+package chapter04;
 
 /**
  * adapter
  * 适配器模式
  *
- * Motivation
- * 动机
+ * bridge
+ * 桥接模式
  *
- *      sometimes a toolkit class that's designed for reuse isn't reuse isn't reusable only because its interface doesn't match the domain-specific interface an application requires
- *      有时工具包提供的类的接口不满足我们使用的接口的声明
+ * composite
+ * 组合模式
  *
- * Applicability
- * 适用场景
+ * decorator
+ * 装饰器模式
  *
- *      you want to reuse an existing class, ant its interface does not match the one you need
- *      你想使用一个已经存在的类,但是他的接口不满足现在需要
+ * facade
+ * 外观模式
  *
- *  Consequences
- *  特点
+ * flyweight
+ * 享元模式
  *
- *      provide hooks for subclasses
- *      提供了子类交互定义
- *      connect parallel class hierarchies
- *      平行的连接子类的继承，参考书中的图形理解，跟上面一条有直接关系
- *      pluggable adapters: a class is more reusable when you minimize the assumptions other class must make to use it
- *      插件适配：类越小越有助于适配
- *      using two-way adapters to provide transparency: class adapter and object adapter
- *      有两种方式提供使用:类适配器和对象适配器
- *
+ * proxy
+ * 代理模式
  *
  */
-public class Adapter {
+public class Chapter04 {
 }
