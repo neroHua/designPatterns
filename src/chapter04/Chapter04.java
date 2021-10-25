8c34c41e090a7f02b7499bc21c93fd1d82755c28
neroHua <38644203+neroHua@users.noreply.github.com>
2021-10-25 14:15:40 +0800

    Create Chapter04.java

---
+++ src/chapter04/Chapter04.java
@@ -0,0 +1,25 @@
+package chapter04;
+
+/**
+ * adapter
+ * 适配器模式
+ *
+ * bridge
+ * 桥接模式
+ *
+ * composite
+ * 组合模式
+ *
+ * decorator
+ * 装饰器模式
+ *
+ * facade
+ * 外观模式
+ *
+ * proxy
+ * 代理模式
+ *
+ *
+ */
+public class Chapter04 {
+}
