ecb0e0b9bc8c8384a628d12f1a3c79f24b4c808e
neroHua <2676337735@qq.com>
2021-11-27 14:04:11 +0800

    DiscussionOfCreationalPatterns

---
+++ src/chapter03/DiscussionOfCreationalPatterns.java
@@ -0,0 +1,10 @@
+package chapter03;
+
+/**
+ *  there are two common ways to parameterize a system.
+ *  one easy way is to subclass the class that creates the object: factory method
+ *  other complex way relies more on object composition: abstract factory, builder, prototype
+ *
+ */
+public class DiscussionOfCreationalPatterns {
+}
