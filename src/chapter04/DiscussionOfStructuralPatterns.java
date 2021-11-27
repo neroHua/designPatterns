8acb6acac5538295015e6746f0c0a3cc0f89fda9
neroHua <2676337735@qq.com>
2021-11-27 13:54:46 +0800

    add DiscussionOfStructuralPatterns

---
+++ src/chapter04/DiscussionOfStructuralPatterns.java
@@ -0,0 +1,16 @@
+package chapter04;
+
+/**
+ *  adapter vs bridge vs facade
+ *      adapter: two interface and new one reuse part of old one
+ *      bridge: one interface and its implementations vary independent
+ *      facade: one new interface cover other interfaces
+ *
+ *  composite vs decorator vs proxy
+ *      composite: different components into tree or table and client use them uniformly.
+ *      decorator: dynamic add responsibilities to components without subclassing
+ *      proxy: control object access
+ *
+ */
+public class DiscussionOfStructuralPatterns {
+}
