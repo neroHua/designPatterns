3151a03fe88c51d03c5015f0e1adb33246f0e7c9
neroHua <2676337735@qq.com>
2021-12-05 14:30:57 +0800

    add DiscussionOfBehavioralPatterns

---
+++ src/chapter05/DiscussionOfBehavioralPatterns.java
@@ -0,0 +1,23 @@
+package chapter05;
+
+/**
+ *  encapsulating
+ *      mediator
+ *      iterator
+ *      state
+ *      strategy
+ *
+ *  objects as arguments
+ *      chainOfResponsibility
+ *      command
+ *      observer
+ *      visitor
+ *
+ *  decoupling Senders and Receivers
+ *      chainOfResponsibility
+ *      command
+ *      observer
+ *
+ */
+public class DiscussionOfBehavioralPatterns {
+}
