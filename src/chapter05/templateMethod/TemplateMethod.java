217212feae8962dc7aa0715e3c95224780c3c6bd
neroHua <2676337735@qq.com>
2021-12-05 13:44:52 +0800

    add template method document

---
+++ src/chapter05/templateMethod/TemplateMethod.java
@@ -0,0 +1,30 @@
+package chapter05.templateMethod;
+
+/**
+ * template method
+ * 模板方法模式
+ *
+ *
+ * Intent
+ * 意图
+ *
+ *      define the skeleton of an algorithm in an operation, deferring some steps to subclasses.
+ *      在某个方法内定义算法的步骤, 有不同实现方式的步骤的通过子类来实现
+ *
+ * Applicability
+ * 适用场景
+ *
+ *      to implement the invariant parts of an algorithm once and leave it up to subclasses to implement the behavior that can vary.
+ *      一次实现算法中不变的部分，将变化的部分交给子类实现
+ *      to control subclasses extensions
+ *      控制子类的扩展
+ *
+ *  Consequences
+ *  特点
+ *
+ *      base technique for code reuse
+ *      重用代码的基本方法
+ *
+ */
+public class TemplateMethod {
+}
