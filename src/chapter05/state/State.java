18fd5010f7d6846715eecdc6a1aab5631fd8e3b6
neroHua <2676337735@qq.com>
2021-12-05 13:03:57 +0800

    add state document

---
+++ src/chapter05/state/State.java
@@ -0,0 +1,30 @@
+package chapter05.state;
+
+/**
+ * state
+ * 状态模式
+ *
+ * Intent
+ * 意图
+ *      allow an object to alter its behavior when its internal state changes. the object will appear to change its class.
+ *      允许一个对象改变他的行为，当它内部状态改变时。通常对象将会改变他的类
+ *
+ * Applicability
+ * 适用场景
+ *
+ *      an object's behavior depends on its state
+ *      对象的行为取决于对象的状态
+ *      operation have large, multipart conditional statements that depend on the object's state.
+ *      操作很多,而且条件很多,而这些依赖于对象的状态
+ *
+ *  Consequences
+ *  特点
+ *
+ *      it localizes state-specific behavior and partitions behavior for different states
+ *      行为根据状态进行了划分
+ *      it makes state transitions explicit
+ *      显式的改变状态
+ *
+ */
+public class State {
+}
