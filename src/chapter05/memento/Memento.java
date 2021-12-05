87180da529c0072ab347214c5b74260b8e134e22
neroHua <2676337735@qq.com>
2021-12-05 12:03:43 +0800

    add memento document

---
+++ src/chapter05/memento/Memento.java
@@ -0,0 +1,30 @@
+package chapter05.memento;
+
+/**
+ * memento
+ * 备忘录模式
+ *
+ * Intent
+ * 意图
+ *      without violating encapsulation, capture and externalize an object's internal state so that the object can be restored to this state later.
+ *      不违反封装,捕获并外部化对象的内部状态,因此可以使对象重新装载之前的状态
+ *
+ * Applicability
+ * 适用场景
+ *
+ *      a snapshot of an object's state must be saved so that it can be restored to that state later
+ *      对象的快照必须存储下来,为了以后可以重新装载对象的状态
+ *
+ *  Consequences
+ *  特点
+ *
+ *      preserving encapsulation boundaries
+ *      保护封装
+ *      it simplifies originator
+ *      简化封装
+ *      using memento might be expensive
+ *      性能可能比较低
+ *
+ */
+public class Memento {
+}
